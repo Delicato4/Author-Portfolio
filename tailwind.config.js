7be2c4431520476e76d8f2d27423bfc135491a82
Amin <aminsahrae4@gmail.com>
2025-03-24 17:17:55 +0330

    header added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  screen:{
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl:' 1280px',
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
